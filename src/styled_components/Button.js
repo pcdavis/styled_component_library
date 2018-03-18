060db73c735123e327f4bd994924a053ce375acb
pcdavis <pcdavis@users.noreply.github.com>
2018-03-18 10:22:49 -0600

    first commit

---
+++ src/styled_components/Button.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export default styled.button`
+
+background-color: ${ props => (props.primary ? "#4caf50" : "#0008CBA" )};
+border: none;
+color: white;
+padding: 15px 32px;
+text-align: center;
+text-decoration: none;
+display: inline-block;
+font-size: 16px;
+
+`;
